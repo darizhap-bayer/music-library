892afdecf8118f1e6150f0cca9bc3860cc7276b9
Darizhap Tsymzhitov <darizhap.tsymzhitov.ext@bayer.com>
2026-03-01 21:52:39 +0600

    Refactor SongServiceClient to use DiscoveryClient for dynamic service URL resolution and remove hardcoded configuration

--- resource-service/src/main/java/com/example/resourceservice/client/SongServiceClient.java
+++ resource-service/src/main/java/com/example/resourceservice/client/SongServiceClient.java
@@ -2,7 +2,7 @@ package com.example.resourceservice.client;
 
 import com.example.resourceservice.dto.CreateSongResponse;
 import com.example.resourceservice.dto.SongMetadataRequest;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -10,19 +10,28 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class SongServiceClient {
   private final RestTemplate restTemplate = new RestTemplate();
+  private final DiscoveryClient discoveryClient;
 
-  @Value("${song-service.url}")
-  private String songServiceUrl;
+  public SongServiceClient(DiscoveryClient discoveryClient) {
+    this.discoveryClient = discoveryClient;
+  }
+
+  private String getSongServiceBaseUrl() {
+    return discoveryClient.getInstances("song-service").stream()
+        .findFirst()
+        .map(instance -> instance.getUri().toString())
+        .orElseThrow(() -> new IllegalStateException("song-service not found in Eureka"));
+  }
 
   public CreateSongResponse createSongMetadata(SongMetadataRequest request) {
-    String url = songServiceUrl + "/songs";
+    String url = getSongServiceBaseUrl() + "/songs";
     ResponseEntity<CreateSongResponse> response =
         restTemplate.postForEntity(url, request, CreateSongResponse.class);
     return response.getBody();
   }
 
   public void deleteSongs(String csvIds) {
-    String url = songServiceUrl + "/songs?id=" + csvIds;
+    String url = getSongServiceBaseUrl() + "/songs?id=" + csvIds;
     restTemplate.delete(url);
   }
 }
